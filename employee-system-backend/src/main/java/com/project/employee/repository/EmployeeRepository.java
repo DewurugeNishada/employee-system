d54ed42621223c4d45a3472fb905c47fdd23f85e
dewuruge <dewurugekalpadeep@gmail.com>
2023-12-29 23:22:30 +0530

    half

---
+++ employee-system-backend/src/main/java/com/project/employee/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.project.employee.repository;
+
+import com.project.employee.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
+
+
+
+}
